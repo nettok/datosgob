cb8d16169ae4bf921cb08255e3db848eac490e7a
Ernesto Menéndez <pyalec@gmail.com>
2015-06-10 11:16:58 -0600

    patrones: waitForResultTable y waitForResultTableUpdate

--- gc-adjudicaciones-recolector/src/main/scala/ConcursosAdjudicados.scala
+++ gc-adjudicaciones-recolector/src/main/scala/ConcursosAdjudicados.scala
@@ -8,9 +8,28 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 
 
-class TablaResultado private (tabla: WebElement, filaTitulo: WebElement, filaPagineo: WebElement) {
-  def this(browser: WebDriver, timeout: FiniteDuration) {
-    this(null, null, null, null)
+case class TablaResultado(tabla: WebElement, filaTitulo: WebElement, filaPagineo: WebElement) {
+  val columnaFecha = filaTitulo.findElement(By.tagName("td"))
+  val paginaActual = filaPagineo.findElements(By.tagName("span")).get(1)
+}
+
+
+object B {
+  def waitForResultTable(browser: WebDriver, timeout: FiniteDuration): TablaResultado = {
+    val tablaResultado = new WebDriverWait(browser, timeout.toSeconds).until(
+      ExpectedConditions.presenceOfElementLocated(By.id("MasterGC_ContentBlockHolder_dgResultado")))
+
+    val filaTitulo = tablaResultado.findElement(By.className("TablaTitulo"))
+    val filaPagineo = tablaResultado.findElement(By.className("TablaPagineo"))
+
+    TablaResultado(tablaResultado, filaTitulo, filaPagineo)
+  }
+
+  def waitForResultTableUpdate(browser: WebDriver, timeout: FiniteDuration, oldTablaResultado: TablaResultado): TablaResultado = {
+    new WebDriverWait(browser, timeout.toSeconds).until(
+      ExpectedConditions.stalenessOf(oldTablaResultado.tabla))
+
+    waitForResultTable(browser, 1.second)
   }
 }
 
@@ -30,47 +49,34 @@ object ConcursosAdjudicados extends App {
   val opcion1Id = "MasterGC_ContentBlockHolder_rdbOpciones_0"
   val opcion1Input = browser.findElement(By.id(opcion1Id))
   val opcion1Label = browser.findElement(By.cssSelector(s"""label[for="$opcion1Id"]"""))
-  assert(opcion1Label.getText == "Opción 1: Buscar TODAS las adjudicaciones")
+  assert(opcion1Label.getText == "Opción 1: Buscar TODAS las adjudicaciones", "Esta debe ser la opcion 1")
 
   // Realizar consulta haciendo clic en esta opcion
   // y esperar a que se actualice la pagina con el resultado (ajax)
 
   opcion1Input.click()
 
-  val tablaResultadoId = "MasterGC_ContentBlockHolder_dgResultado"
-  val tablaResultado = new WebDriverWait(browser, waitTimeoutSeconds).until(
-    ExpectedConditions.presenceOfElementLocated(By.id(tablaResultadoId)))
-
-  // Verificar que estamos en la pagina 1
-
-  val filaPagineo = tablaResultado.findElement(By.className("TablaPagineo"))
-  val paginaActual = filaPagineo.findElements(By.tagName("span")).get(1)
-  assert(paginaActual.getText == "1")
-
-  // Verificar que el resultado esta ordenado por "Fecha de adjudicación" en orden descendente
-
-  val filaTitulo = tablaResultado.findElement(By.className("TablaTitulo"))
-  val columnaFecha = filaTitulo.findElement(By.tagName("td"))
-  assert(columnaFecha.getText == "Fecha de adjudicación▼")
+  var tabla = B.waitForResultTable(browser, waitTimeout)
+  assert(tabla.paginaActual.getText == "1", "Tenemos que estar en la primera pagina")
+  assert(tabla.columnaFecha.getText == "Fecha de adjudicación▼",
+    """El resultado debe estar ordenado por "Fecha de adjudicación" en orden descendente""")
 
   // Ordenar el resultado en orden ascendente
   // y esperar a que se actualice el resultado (ajax)
 
-  val cambiarOrden = columnaFecha.findElement(By.tagName("a"))
+  var cambiarOrden = tabla.columnaFecha.findElement(By.tagName("a"))
   cambiarOrden.click()
 
-  new WebDriverWait(browser, waitTimeoutSeconds).until(
-    ExpectedConditions.textToBePresentInElement(columnaFecha, "Fecha de adjudicación▲"))
+  tabla = B.waitForResultTableUpdate(browser, waitTimeout, tabla)
+  assert(tabla.columnaFecha.getText == "Fecha de adjudicación▲", "El resultado debe estar en orden ascendente")
 
-  // xxx
+  // Obtener registros de la pagina actual
 
-  val fila1 = tablaResultado.findElement(By.className("TablaFila1"))
+  // TODO
+  var fila1 = tabla.tabla.findElement(By.className("TablaFila1"))
   println(fila1.getText)
 
-  def resultTableUpdated(): () = {
-  }
+  // TODO: Pasar a la siguiente pagina si existe
 
-  // yyy
-
-  val tr = new TablaResultado(browser, waitTimeout)
+  // TODO: Ir a: Obtener registro de la pagina actual
 }
