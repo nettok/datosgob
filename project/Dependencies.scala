fa7804f113b7782d3a633be0c2af595320d5cbeb
Ernesto Menéndez <pyalec@gmail.com>
2015-08-02 11:09:01 -0600

    actualizar algunas dependencias

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,10 +9,10 @@ object Dependencies {
   val akkaHttp = "com.typesafe.akka" %% "akka-http-experimental" % "1.0"
   val akkaHttpJson = "de.heikoseeberger" %% "akka-http-play-json" % "1.0.0"
   val slick = "com.typesafe.slick" %% "slick" % "3.0.0"
-  val sqlite = "org.xerial" % "sqlite-jdbc" % "3.8.10.1"
+  val sqlite = "org.xerial" % "sqlite-jdbc" % "3.8.11"
   val hikaricp = "com.zaxxer" % "HikariCP" % "2.3.9"
   val postgres = "org.postgresql" % "postgresql" % "9.4-1201-jdbc41"
-  val selenium = "org.seleniumhq.selenium" % "selenium-java" % "2.46.0"
+  val selenium = "org.seleniumhq.selenium" % "selenium-java" % "2.47.1"
   val phantomjsdriver = "com.codeborne" % "phantomjsdriver" % "1.2.1"
   val logback = "ch.qos.logback" % "logback-classic" % "1.1.3"
 
